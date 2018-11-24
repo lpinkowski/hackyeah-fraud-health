7db2e49685447c18b542bc9a011293ee6989056f
karol.krzyzak <karol.krzyzak@gmail.com>
2018-11-24 13:11:58 +0100

    init

---
+++ src/main/java/pl/hackyeah/nhf/health/fraud/oszusci/OszusciApplication.java
@@ -0,0 +1,12 @@
+package pl.hackyeah.nhf.health.fraud.oszusci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OszusciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OszusciApplication.class, args);
+	}
+}
